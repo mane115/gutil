44c42ca3159ffad03535e6e68a40821ba2673dbc
gh <guohao0223@gmail.com>
2017-12-15 12:00:13 +0800

    adapt browser

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
  * util 为了兼容较旧的node版本: >= 4.6.0(支持promise)，所以大部分使用es5语法编写
  */
 'use strict'
-let Promise = require('bluebird');
-module.exports = {
+let util = {
 	Http: require('ghttp'),
 	parse: require('./lib/parse'),
-	fs: Promise.promisifyAll(require('fs')),
+	// fs: Promise.promisifyAll(require('fs')),
+}
+
+// if runtime is node, promisify fs and export util 
+if (process && process.title === 'node' && require) {
+	let Promise = require('bluebird');
+	util.fs = Promise.promisifyAll(require('fs'));
+}
+if (module && module.exports) {
+	return module.exports = util;
 }
